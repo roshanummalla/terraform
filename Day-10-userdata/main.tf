e0f5b706155f86fb90a1f607f02967350bfb8343
roshanummalla <ummallaroshan@gmail.com>
2025-05-06 21:09:26 +0530

    Update main.tf

--- Day-10-userdata/main.tf
+++ Day-10-userdata/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "name" {
-    ami = "ami-085ad6ae776d8f09c"
+    ami = "ami-0f88e80871fd81e91"
     instance_type = "t2.micro"
     # key_name = "ec2test"
     availability_zone = "us-east-1a"
